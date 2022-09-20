77e7d0e554ace0edf6e174841119ec85abb1e60c
rngustj9139 <mnb9139@khu.ac.kr>
2022-09-21 05:06:23 +0900

    Setting first environment

---
+++ src/main/java/koo/MicroServiceCatalogService/MicroServiceCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package koo.MicroServiceCatalogService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koo/MicroServiceCatalogService/MicroServiceCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package koo.MicroServiceCatalogService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
